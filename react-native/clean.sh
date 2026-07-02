6f73c5d00763b74717357d3bf104c7df57be5465
Taner Sener <tanersener@gmail.com>
2026-07-02 23:13:01 +0300

    fix the paths

--- react-native/clean.sh
+++ react-native/clean.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm -rf ../react-native/node_modules
-rm -rf ../react-native/ios/build
-rm -rf ../react-native/ios/Pods
-rm -rf ../react-native/android/.gradle
-rm -rf ../react-native/android/app/build
+rm -rf node_modules
+rm -rf ios/build
+rm -rf ios/Pods
+rm -rf android/.gradle
+rm -rf android/app/build
+rm -rf android/app/.cxx
